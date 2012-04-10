45ca553e300e30fcbaf31519ba6ddc0801f0087b
Kyle Conroy <kyle.j.conroy@gmail.com>
2012-04-09 18:17:58 -0700

    Add authentication header

--- web.py
+++ web.py
@@ -1,7 +1,45 @@
-from flask import Flask
+from flask import Flask, request, Response
+import redis
+import os, urlparse
+from functools import wraps
+
+
+if os.environ.has_key('REDISTOGO_URL'):
+    urlparse.uses_netloc.append('redis')
+    url = urlparse.urlparse(os.environ['REDISTOGO_URL'])
+    r = redis.Redis(host=url.hostname, port=url.port, 
+                    db=0, password=url.password)
+else:
+    r = redis.Redis()
+
+
+def check_auth(username, password):
+    """This function is called to check if a username /
+    password combination is valid.
+    """
+    return username == 'admin' and password == 'secret'
+
+
+def authenticate():
+    """Sends a 401 response that enables basic auth"""
+    return Response(
+    'Could not verify your access level for that URL.\n'
+    'You have to login with proper credentials', 401,
+    {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
+
+def authenticated(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            return authenticate()
+        return f(*args, **kwargs)
+    return decorated
 
 app = Flask(__name__)
 
 @app.route("/")
+@authenticated
 def index():
     pass
